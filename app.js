6cb65a50312bbd89e2cd1743c8a73d5c4222f85f
Douglas Mendes <lixao_9@hotmail.com>
2022-08-25 18:13:08 -0300

    feat: add elements correctly, and add a new history with ENTER keyboard

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ var initMethods = {
     mode: 'no-cors',
     cache: 'default'
 }
+let history = []
+const cep = document.querySelector('#cep').value
+const cepInput = document.querySelector('#cep')
+
+const limpaInput = () =>{
+    cep.innerHTML=" "
+}
+
+cepInput.addEventListener('keyup', e=> {
+    if(e.key === 'Enter'){
+        search()
+        //limpaInput()
+    }
+})
+
+
 
 
 const search = async () => {
@@ -17,14 +33,24 @@ const search = async () => {
         response.json().then(function(data){
             console.log(data)
             showResults(data)
+            document.querySelector('#cep').value = ''
+
         })
     })
+
+    
 }
 
 const showResults = (data) => {
     let info = document.querySelector('.information')
+    let history_el = document.querySelector('.history-cep')
+    
+    
+    history.push(data)
+    console.log(history)
 
     info.innerHTML = `<h1>INFORMATIONS</h1>
+                      <label> Bairro</label>
                       <input type="text" value="${data.cep}" readonly id="bairro"/>
                       <input type="text" value="${data.bairro}" readonly id="bairro"/>
                       <input type="text" value="${data.complemento}" readonly id="bairro"/>
@@ -32,6 +58,21 @@ const showResults = (data) => {
                       <input type="text" value="${data.logradouro}" readonly id="bairro"/>
                       <input type="text" value="${data.uf}" readonly id="bairro"/>
                       `
+
+    history_el.innerHTML = history.map(getElement)
+    function getElement(item,index){
+                           return  [`<div class="element">
+                                <p>${index+1} </>
+                                <div class="components">
+                                    <div class="cep-title">${item.cep}</div>
+                                    <div class="cep-information">${item.localidade} - ${item.uf} </div>
+                                </div>
+                                <div class="buttons-cep">
+                                <button> delete </button>
+                                <button> ver </button>
+                                </div>
+                            </div>`].join(" ")
+    }
 }
 
 
