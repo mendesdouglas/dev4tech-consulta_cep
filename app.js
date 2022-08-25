ddb610d02a619fd251d99e94232f94e70aaa86ca
Douglas Mendes <lixao_9@hotmail.com>
2022-08-25 12:32:10 -0300

    feat: include elements to show when a user get CEP result

--- app.js
+++ app.js
@@ -16,9 +16,25 @@ const search = async () => {
     await fetch(url).then(function(response){
         response.json().then(function(data){
             console.log(data)
+            showResults(data)
         })
     })
 }
+
+const showResults = (data) => {
+    let info = document.querySelector('.information')
+
+    info.innerHTML = `<h1>INFORMATIONS</h1>
+                      <input type="text" value="${data.cep}" readonly id="bairro"/>
+                      <input type="text" value="${data.bairro}" readonly id="bairro"/>
+                      <input type="text" value="${data.complemento}" readonly id="bairro"/>
+                      <input type="text" value="${data.localidade}" readonly id="bairro"/>
+                      <input type="text" value="${data.logradouro}" readonly id="bairro"/>
+                      <input type="text" value="${data.uf}" readonly id="bairro"/>
+                      `
+}
+
+
 
 
 
